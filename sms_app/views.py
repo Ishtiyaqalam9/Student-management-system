3676e9fb03e5c0b95c9e9bf2653a7f0709a306ae
Ishtiyaq Alam <ishtiyaqalam9562@gmail.com>
2026-05-24 23:14:31 +0600

    part2 & part3

--- sms_app/views.py
+++ sms_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from datetime import datetime
 
+def home(request):
+    context = {
+        'name': 'Ishtiyaq Alam',
+        'course': 'Django Internship',
+        'today': datetime.now(),
+        'numbers': [10, 20, 30, 40]
+    }
+
+    return render(request, 'home.html', context)
+
+
+def about(request):
+    context = {
+        'description': 'This is Django templating practice project.',
+        'skills': ['Inheritance', 'Filters', 'Tags']
+    }
+
+    return render(request, 'about.html', context)
 
