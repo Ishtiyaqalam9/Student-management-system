42f1e079ab9599bdd494119e890e34c758fbd9ba
Ishtiyaq Alam <ishtiyaqalam9562@gmail.com>
2026-06-20 23:29:34 +0600

    Added Todo List feature

--- sms_app/urls.py
+++ sms_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('delete/<int:task_id>/' ,  views.delete_task, name= 'delete_task'),
 ]
--- sms_app/views.py
+++ sms_app/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from datetime import datetime
+from .models import Todo
 
 def home(request):
+    if request.method == "POST":
+        task = request.POST.get('task')
+
+        if task:
+            Todo.objects.create(title=task)
+
+        return redirect('home')
+
+    tasks = Todo.objects.all().order_by('-id')
+
     context = {
         'name': 'Ishtiyaq Alam',
         'course': 'Django Internship',
         'today': datetime.now(),
-        'numbers': [10, 20, 30, 40]
+        'numbers': [10, 20, 30, 40],
+        'tasks': tasks
     }
 
     return render(request, 'home.html', context)
@@ -20,3 +32,7 @@ def about(request):
 
     return render(request, 'about.html', context)
 
+
+def delete_task(request, task_id):
+    Todo.objects.filter(id=task_id).delete()
+    return redirect('home')
